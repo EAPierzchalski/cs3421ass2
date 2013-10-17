041bfb460d013f60343afebd12eac054c35d5f9a
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2013-10-17 19:30:50 +1100

    texture shaders

--- src/main/resources/shaders/fragment/simple_fragment.glsl
+++ src/main/resources/shaders/fragment/simple_fragment.glsl
@@ -1,5 +1,8 @@
 //FRAGMENT SHADER
 
+varying vec2 texture_coordinate;
+uniform sampler2D texture_sampler;
+
 void main(void) {
-    gl_FragColor = gl_Color;
+    gl_FragColor = texture2D(texture_sampler, texture_coordinate);
 }
--- src/main/resources/shaders/vertex/simple_vertex.glsl
+++ src/main/resources/shaders/vertex/simple_vertex.glsl
@@ -1,5 +1,8 @@
 //VERTEX SHADER
 
+varying vec2 texture_coordinate;
+
 void main(void) {
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    texture_coordinate = vec2(gl_MultiTexCoord0);
 }
